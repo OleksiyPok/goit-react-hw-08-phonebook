106f0225fadafcb0412ac88a51a71be5e650a394
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-11-20 13:05:34 +0100

    login page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from 'react-router';
 import { Layout } from './Layout';
 import { HomePage } from 'pages/HomePage';
 // import { RegistrationPage } from 'pages/RegistrationPage';
-// import { LoginPage } from 'pages/LoginPage';
+import { LoginPage } from 'pages/LoginPage';
 // import { ContactsPage } from 'pages/ContactsPage';
-// import routes from 'routes';
+import routes from 'routes';
 
 // import { PrivateRoute } from './PrivateRoute';
-// import { PublicRoute } from './PublicRoute';
+import { PublicRoute } from './PublicRoute';
 
 import { ToastContainer } from 'react-toastify';
 import { toastParams } from 'components/toastify';
@@ -28,14 +28,14 @@ const App = () => {
               </PublicRoute>
             }
           /> */}
-          {/* <Route
+          <Route
             path={routes.LOGIN_PAGE}
             element={
               <PublicRoute>
                 <LoginPage />
               </PublicRoute>
             }
-          /> */}
+          />
           {/* <Route
             path={routes.CONTACTS_PAGE}
             element={
