9e9a37afcc85107abbc2b95b764e38b3185488ad
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-12-23 19:09:26 +0100

    extra export has been deleted

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,3 @@ export const nameRegex =
 
 export const phoneRegex =
   '+?d{1,4}?[.-s]?(?d{1,3}?)?[.-s]?d{1,4}[.-s]?d{1,4}[.-s]?d{1,9}';
-
-export const constants = () => {
-  nameRegex, phoneRegex;
-};
